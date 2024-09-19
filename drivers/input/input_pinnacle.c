0759bf65b03f249261fb7e4a13421719b6a18c5d
Peter Johanson <peter@peterjohanson.com>
2024-09-19 01:54:53 -0600

    feat: Various driver improvements.
    * Multiple instance fixes.
    * Edge sensitivity tuning support.
    * Button/signed data parsing fixes.

--- drivers/input/input_pinnacle.c
+++ drivers/input/input_pinnacle.c
@@ -1,8 +1,9 @@
 #define DT_DRV_COMPAT cirque_pinnacle
 
+#include <zephyr/dt-bindings/input/input-event-codes.h>
 #include <zephyr/init.h>
 #include <zephyr/input/input.h>
-#include <zephyr/dt-bindings/input/input-event-codes.h>
+#include <zephyr/pm/device.h>
 
 #include <zephyr/logging/log.h>
 
@@ -13,7 +14,33 @@ LOG_MODULE_REGISTER(pinnacle, CONFIG_INPUT_LOG_LEVEL);
 static int pinnacle_seq_read(const struct device *dev, const uint8_t addr, uint8_t *buf,
                              const uint8_t len) {
     const struct pinnacle_config *config = dev->config;
-#if DT_INST_ON_BUS(0, spi)
+    return config->seq_read(dev, addr, buf, len);
+}
+static int pinnacle_write(const struct device *dev, const uint8_t addr, const uint8_t val) {
+    const struct pinnacle_config *config = dev->config;
+    return config->write(dev, addr, val);
+}
+
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c)
+
+static int pinnacle_i2c_seq_read(const struct device *dev, const uint8_t addr, uint8_t *buf,
+                                 const uint8_t len) {
+    const struct pinnacle_config *config = dev->config;
+    return i2c_burst_read_dt(&config->bus.i2c, PINNACLE_READ | addr, buf, len);
+}
+
+static int pinnacle_i2c_write(const struct device *dev, const uint8_t addr, const uint8_t val) {
+    const struct pinnacle_config *config = dev->config;
+    return i2c_reg_write_byte_dt(&config->bus.i2c, PINNACLE_WRITE | addr, val);
+}
+
+#endif // DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c)
+
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
+
+static int pinnacle_spi_seq_read(const struct device *dev, const uint8_t addr, uint8_t *buf,
+                                 const uint8_t len) {
+    const struct pinnacle_config *config = dev->config;
     uint8_t tx_buffer[len + 3], rx_dummy[3];
     tx_buffer[0] = PINNACLE_READ | addr;
     memset(&tx_buffer[1], PINNACLE_AUTOINC, len + 2);
@@ -21,16 +48,12 @@ static int pinnacle_seq_read(const struct device *dev, const uint8_t addr, uint8
     const struct spi_buf tx_buf[2] = {
         {
             .buf = tx_buffer,
-            .len = 3,
-        },
-        {
-            .buf = &tx_buffer[3],
-            .len = len,
+            .len = len + 3,
         },
     };
     const struct spi_buf_set tx = {
         .buffers = tx_buf,
-        .count = 2,
+        .count = 1,
     };
     struct spi_buf rx_buf[2] = {
         {
@@ -46,17 +69,13 @@ static int pinnacle_seq_read(const struct device *dev, const uint8_t addr, uint8
         .buffers = rx_buf,
         .count = 2,
     };
-    int ret = spi_transceive_dt(&config->bus, &tx, &rx);
+    int ret = spi_transceive_dt(&config->bus.spi, &tx, &rx);
 
     return ret;
-#elif DT_INST_ON_BUS(0, i2c)
-    return i2c_burst_read_dt(&config->bus, PINNACLE_READ | addr, buf, len);
-#endif
 }
 
-static int pinnacle_write(const struct device *dev, const uint8_t addr, const uint8_t val) {
+static int pinnacle_spi_write(const struct device *dev, const uint8_t addr, const uint8_t val) {
     const struct pinnacle_config *config = dev->config;
-#if DT_INST_ON_BUS(0, spi)
     uint8_t tx_buffer[2] = {PINNACLE_WRITE | addr, val};
     uint8_t rx_buffer[2];
 
@@ -78,21 +97,22 @@ static int pinnacle_write(const struct device *dev, const uint8_t addr, const ui
         .count = 1,
     };
 
-    const int ret = spi_transceive_dt(&config->bus, &tx, &rx);
-
-    if (rx_buffer[1] != PINNACLE_FILLER) {
-        LOG_ERR("bad ret val %d", rx_buffer[1]);
-        return -EIO;
-    }
+    const int ret = spi_transceive_dt(&config->bus.spi, &tx, &rx);
 
     if (ret < 0) {
         LOG_ERR("spi ret: %d", ret);
     }
+
+    if (rx_buffer[1] != PINNACLE_FILLER) {
+        LOG_ERR("bad ret val %d - %d", rx_buffer[0], rx_buffer[1]);
+        return -EIO;
+    }
+
+    k_usleep(50);
+
     return ret;
-#elif DT_INST_ON_BUS(0, i2c)
-    return i2c_reg_write_byte_dt(&config->bus, PINNACLE_WRITE | addr, val);
-#endif
 }
+#endif // DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
 
 static void set_int(const struct device *dev, const bool en) {
     const struct pinnacle_config *config = dev->config;
@@ -112,7 +132,6 @@ static int pinnacle_clear_status(const struct device *dev) {
     return ret;
 }
 
-#if 0
 static int pinnacle_era_read(const struct device *dev, const uint16_t addr, uint8_t *val) {
     int ret;
 
@@ -160,7 +179,6 @@ static int pinnacle_era_read(const struct device *dev, const uint16_t addr, uint
 
     return ret;
 }
-#endif
 
 static int pinnacle_era_write(const struct device *dev, const uint16_t addr, uint8_t val) {
     int ret;
@@ -218,6 +236,9 @@ static void pinnacle_report_data(const struct device *dev) {
         LOG_ERR("read status: %d", ret);
         return;
     }
+
+    LOG_HEXDUMP_DBG(packet, 1, "Pinnacle Status1");
+
     if (!(packet[0] & PINNACLE_STATUS1_SW_DR)) {
         return;
     }
@@ -226,12 +247,23 @@ static void pinnacle_report_data(const struct device *dev) {
         LOG_ERR("read packet: %d", ret);
         return;
     }
+
+    LOG_HEXDUMP_DBG(packet, 3, "Pinnacle Packets");
+
     struct pinnacle_data *data = dev->data;
     uint8_t btn = packet[0] &
                   (PINNACLE_PACKET0_BTN_PRIM | PINNACLE_PACKET0_BTN_SEC | PINNACLE_PACKET0_BTN_AUX);
-    int16_t dx = (int16_t)(int8_t)packet[1];
-    int16_t dy = (int16_t)(int8_t)packet[2];
-    LOG_DBG("button: %d, dx: %d dy: %d", btn, dx, dy);
+
+    int8_t dx = (int8_t)packet[1];
+    int8_t dy = (int8_t)packet[2];
+
+    if (packet[0] & PINNACLE_PACKET0_X_SIGN) {
+        WRITE_BIT(dx, 7, 1);
+    }
+    if (packet[0] & PINNACLE_PACKET0_Y_SIGN) {
+        WRITE_BIT(dy, 7, 1);
+    }
+
     if (data->in_int) {
         LOG_DBG("Clearing status bit");
         ret = pinnacle_clear_status(dev);
@@ -281,14 +313,100 @@ static int pinnacle_adc_sensitivity_reg_value(enum pinnacle_sensitivity sensitiv
     }
 }
 
+static int pinnacle_tune_edge_sensitivity(const struct device *dev) {
+    const struct pinnacle_config *config = dev->config;
+    int ret;
+
+    uint8_t x_val;
+    ret = pinnacle_era_read(dev, PINNACLE_ERA_REG_X_AXIS_WIDE_Z_MIN, &x_val);
+    if (ret < 0) {
+        LOG_WRN("Failed to read X val");
+        return ret;
+    }
+
+    LOG_WRN("X val: %d", x_val);
+
+    uint8_t y_val;
+    ret = pinnacle_era_read(dev, PINNACLE_ERA_REG_Y_AXIS_WIDE_Z_MIN, &y_val);
+    if (ret < 0) {
+        LOG_WRN("Failed to read Y val");
+        return ret;
+    }
+
+    LOG_WRN("Y val: %d", y_val);
+
+    ret = pinnacle_era_write(dev, PINNACLE_ERA_REG_X_AXIS_WIDE_Z_MIN, config->x_axis_z_min);
+    if (ret < 0) {
+        LOG_ERR("Failed to set X-Axis Min-Z %d", ret);
+        return ret;
+    }
+    ret = pinnacle_era_write(dev, PINNACLE_ERA_REG_Y_AXIS_WIDE_Z_MIN, config->y_axis_z_min);
+    if (ret < 0) {
+        LOG_ERR("Failed to set Y-Axis Min-Z %d", ret);
+        return ret;
+    }
+    return 0;
+}
+
+static int pinnacle_set_adc_tracking_sensitivity(const struct device *dev) {
+    const struct pinnacle_config *config = dev->config;
+
+    uint8_t val;
+    int ret = pinnacle_era_read(dev, PINNACLE_ERA_REG_TRACKING_ADC_CONFIG, &val);
+    if (ret < 0) {
+        LOG_ERR("Failed to get ADC sensitivity %d", ret);
+    }
+
+    val &= 0x3F;
+    val |= pinnacle_adc_sensitivity_reg_value(config->sensitivity);
+
+    ret = pinnacle_era_write(dev, PINNACLE_ERA_REG_TRACKING_ADC_CONFIG, val);
+    if (ret < 0) {
+        LOG_ERR("Failed to set ADC sensitivity %d", ret);
+    }
+    ret = pinnacle_era_read(dev, PINNACLE_ERA_REG_TRACKING_ADC_CONFIG, &val);
+    if (ret < 0) {
+        LOG_ERR("Failed to get ADC sensitivity %d", ret);
+    }
+
+    return ret;
+}
+
+static int pinnacle_force_recalibrate(const struct device *dev) {
+    uint8_t val;
+    int ret = pinnacle_seq_read(dev, PINNACLE_CAL_CFG, &val, 1);
+    if (ret < 0) {
+        LOG_ERR("Failed to get cal config %d", ret);
+    }
+
+    val |= 0x01;
+    ret = pinnacle_write(dev, PINNACLE_CAL_CFG, val);
+    if (ret < 0) {
+        LOG_ERR("Failed to force calibration %d", ret);
+    }
+
+    do {
+        pinnacle_seq_read(dev, PINNACLE_CAL_CFG, &val, 1);
+    } while (val & 0x01);
+
+    return ret;
+}
+
 static int pinnacle_init(const struct device *dev) {
     struct pinnacle_data *data = dev->data;
     const struct pinnacle_config *config = dev->config;
-
-    LOG_WRN("pinnacle start");
-    data->in_int = false;
     int ret;
-    k_msleep(4);
+
+    uint8_t fw_id[2];
+    ret = pinnacle_seq_read(dev, PINNACLE_FW_ID, fw_id, 2);
+    if (ret < 0) {
+        LOG_ERR("Failed to get the FW ID %d", ret);
+    }
+
+    LOG_DBG("Found device with FW ID: 0x%02x, Version: 0x%02x", fw_id[0], fw_id[1]);
+
+    data->in_int = false;
+    k_msleep(10);
     ret = pinnacle_write(dev, PINNACLE_STATUS1, 0); // Clear CC
     if (ret < 0) {
         LOG_ERR("can't write %d", ret);
@@ -306,6 +424,24 @@ static int pinnacle_init(const struct device *dev) {
         LOG_ERR("can't write %d", ret);
         return ret;
     }
+
+    ret = pinnacle_set_adc_tracking_sensitivity(dev);
+    if (ret < 0) {
+        LOG_ERR("Failed to set ADC sensitivity %d", ret);
+        return ret;
+    }
+
+    ret = pinnacle_tune_edge_sensitivity(dev);
+    if (ret < 0) {
+        LOG_ERR("Failed to tune edge sensitivity %d", ret);
+        return ret;
+    }
+    ret = pinnacle_force_recalibrate(dev);
+    if (ret < 0) {
+        LOG_ERR("Failed to force recalibration %d", ret);
+        return ret;
+    }
+
     if (config->sleep_en) {
         ret = pinnacle_write(dev, PINNACLE_SYS_CFG, PINNACLE_SYS_CFG_EN_SLEEP);
         if (ret < 0) {
@@ -314,14 +450,19 @@ static int pinnacle_init(const struct device *dev) {
         }
     }
 
-    ret = pinnacle_era_write(dev, PINNACLE_ERA_REG_TRACKING_ADC_CONFIG,
-                             pinnacle_adc_sensitivity_reg_value(config->sensitivity));
-    if (ret < 0) {
-        LOG_ERR("Failed to set ADC sensitivity %d", ret);
-        return ret;
+    uint8_t packet[1];
+    ret = pinnacle_seq_read(dev, PINNACLE_SLEEP_INTERVAL, packet, 1);
+
+    if (ret >= 0) {
+        LOG_DBG("Default sleep interval %d", packet[0]);
     }
 
-    uint8_t feed_cfg2 = PINNACLE_FEED_CFG2_EN_IM;
+    ret = pinnacle_write(dev, PINNACLE_SLEEP_INTERVAL, 255);
+    if (ret <= 0) {
+        LOG_DBG("Failed to update sleep interaval %d", ret);
+    }
+
+    uint8_t feed_cfg2 = PINNACLE_FEED_CFG2_EN_IM | PINNACLE_FEED_CFG2_EN_BTN_SCRL;
     if (config->no_taps) {
         feed_cfg2 |= PINNACLE_FEED_CFG2_DIS_TAP;
     }
@@ -363,22 +504,43 @@ static int pinnacle_init(const struct device *dev) {
     return 0;
 }
 
+#if IS_ENABLED(CONFIG_PM_DEVICE)
+
+static int pinnacle_pm_action(const struct device *dev, enum pm_device_action action) {
+    switch (action) {
+    case PM_DEVICE_ACTION_SUSPEND:
+        return set_int(dev, false);
+        return 0;
+    case PM_DEVICE_ACTION_RESUME:
+        return set_int(dev, true);
+    default:
+        return -ENOTSUP;
+    }
+}
+
+#endif // IS_ENABLED(CONFIG_PM_DEVICE)
+
 #define PINNACLE_INST(n)                                                                           \
     static struct pinnacle_data pinnacle_data_##n;                                                 \
     static const struct pinnacle_config pinnacle_config_##n = {                                    \
-        .bus = COND_CODE_1(                                                                        \
-            DT_INST_ON_BUS(0, i2c), (I2C_DT_SPEC_INST_GET(0)),                                     \
-            (SPI_DT_SPEC_INST_GET(0,                                                               \
-                                  SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_LINES_SINGLE |        \
-                                      SPI_TRANSFER_MSB | SPI_MODE_CPHA,                            \
-                                  0))),                                                            \
-        .rotate_90 = DT_INST_PROP(0, rotate_90),                                                   \
-        .sleep_en = DT_INST_PROP(0, sleep),                                                        \
-        .no_taps = DT_INST_PROP(0, no_taps),                                                       \
-        .sensitivity = DT_INST_ENUM_IDX_OR(0, sensitivity, PINNACLE_SENSITIVITY_1X),               \
-        .dr = GPIO_DT_SPEC_GET_OR(DT_DRV_INST(0), dr_gpios, {}),                                   \
+        COND_CODE_1(DT_INST_ON_BUS(n, i2c),                                                        \
+                    (.bus = {.i2c = I2C_DT_SPEC_INST_GET(n)}, .seq_read = pinnacle_i2c_seq_read,   \
+                     .write = pinnacle_i2c_write),                                                 \
+                    (.bus = {.spi = SPI_DT_SPEC_INST_GET(n,                                        \
+                                                         SPI_OP_MODE_MASTER | SPI_WORD_SET(8) |    \
+                                                             SPI_TRANSFER_MSB | SPI_MODE_CPHA,     \
+                                                         0)},                                      \
+                     .seq_read = pinnacle_spi_seq_read, .write = pinnacle_spi_write)),             \
+        .rotate_90 = DT_INST_PROP(n, rotate_90),                                                   \
+        .sleep_en = DT_INST_PROP(n, sleep),                                                        \
+        .no_taps = DT_INST_PROP(n, no_taps),                                                       \
+        .x_axis_z_min = DT_INST_PROP_OR(n, x_axis_z_min, 5),                                       \
+        .y_axis_z_min = DT_INST_PROP_OR(n, y_axis_z_min, 4),                                       \
+        .sensitivity = DT_INST_ENUM_IDX_OR(n, sensitivity, PINNACLE_SENSITIVITY_1X),               \
+        .dr = GPIO_DT_SPEC_GET_OR(DT_DRV_INST(n), dr_gpios, {}),                                   \
     };                                                                                             \
-    DEVICE_DT_INST_DEFINE(n, pinnacle_init, NULL, &pinnacle_data_##n, &pinnacle_config_##n,        \
-                          POST_KERNEL, CONFIG_INPUT_INIT_PRIORITY, NULL);
+    PM_DEVICE_DT_INST_DEFINE(n, pinnacle_pm_action);                                               \
+    DEVICE_DT_INST_DEFINE(n, pinnacle_init, PM_DEVICE_DT_INST_GET(n), &pinnacle_data_##n,          \
+                          &pinnacle_config_##n, POST_KERNEL, CONFIG_INPUT_INIT_PRIORITY, NULL);
 
 DT_INST_FOREACH_STATUS_OKAY(PINNACLE_INST)
--- drivers/input/input_pinnacle.h
+++ drivers/input/input_pinnacle.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <zephyr/device.h>
-#include <zephyr/drivers/spi.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/spi.h>
 
 #define PINNACLE_READ 0xA0
 #define PINNACLE_WRITE 0x80
@@ -29,22 +29,23 @@
 #define PINNACLE_FEED_CFG1_DIS_Y BIT(4)
 #define PINNACLE_FEED_CFG1_INV_X BIT(6)
 #define PINNACLE_FEED_CFG1_INV_Y BIT(7)
-#define PINNACLE_FEED_CFG2 0x05             // Contains feed operation and configuration bits.
-#define PINNACLE_FEED_CFG2_EN_IM BIT(0)     // Intellimouse
-#define PINNACLE_FEED_CFG2_DIS_TAP BIT(1)   // Disable all taps
-#define PINNACLE_FEED_CFG2_DIS_SEC BIT(2)   // Disable secondary tap
-#define PINNACLE_FEED_CFG2_DIS_SCRL BIT(3)  // Disable scroll
-#define PINNACLE_FEED_CFG2_DIS_GE BIT(4)    // Disable GlideExtend
-#define PINNACLE_FEED_CFG2_ROTATE_90 BIT(7) // Swap X & Y
-#define PINNACLE_CAL_CFG 0x07               // Contains calibration configuration bits.
-#define PINNACLE_PS2_AUX 0x08               // Contains Data register for PS/2 Aux Control.
-#define PINNACLE_SAMPLE 0x09                // Sample Rate Number of samples generated per second.
-#define PINNACLE_Z_IDLE 0x0A                // Number of Z=0 packets sent when Z goes from >0 to 0.
-#define PINNACLE_Z_SCALER 0x0B              // Contains the pen Z_On threshold.
-#define PINNACLE_SLEEP_INTERVAL 0x0C        // Sleep Interval
-#define PINNACLE_SLEEP_TIMER 0x0D           // Sleep Timer
-#define PINNACLE_AG_PACKET0 0x10            // trackpad Data (Pinnacle AG)
-#define PINNACLE_2_2_PACKET0 0x12           // trackpad Data
+#define PINNACLE_FEED_CFG2 0x05               // Contains feed operation and configuration bits.
+#define PINNACLE_FEED_CFG2_EN_IM BIT(0)       // Intellimouse
+#define PINNACLE_FEED_CFG2_DIS_TAP BIT(1)     // Disable all taps
+#define PINNACLE_FEED_CFG2_DIS_SEC BIT(2)     // Disable secondary tap
+#define PINNACLE_FEED_CFG2_DIS_SCRL BIT(3)    // Disable scroll
+#define PINNACLE_FEED_CFG2_DIS_GE BIT(4)      // Disable GlideExtend
+#define PINNACLE_FEED_CFG2_EN_BTN_SCRL BIT(6) // Enable Button Scroll
+#define PINNACLE_FEED_CFG2_ROTATE_90 BIT(7)   // Swap X & Y
+#define PINNACLE_CAL_CFG 0x07                 // Contains calibration configuration bits.
+#define PINNACLE_PS2_AUX 0x08                 // Contains Data register for PS/2 Aux Control.
+#define PINNACLE_SAMPLE 0x09                  // Sample Rate Number of samples generated per second.
+#define PINNACLE_Z_IDLE 0x0A         // Number of Z=0 packets sent when Z goes from >0 to 0.
+#define PINNACLE_Z_SCALER 0x0B       // Contains the pen Z_On threshold.
+#define PINNACLE_SLEEP_INTERVAL 0x0C // Sleep Interval
+#define PINNACLE_SLEEP_TIMER 0x0D    // Sleep Timer
+#define PINNACLE_AG_PACKET0 0x10     // trackpad Data (Pinnacle AG)
+#define PINNACLE_2_2_PACKET0 0x12    // trackpad Data
 #define PINNACLE_REG_COUNT 0x18
 
 #define PINNACLE_REG_ERA_VALUE 0x1B
@@ -55,6 +56,8 @@
 #define PINNACLE_ERA_CONTROL_READ 0x01
 #define PINNACLE_ERA_CONTROL_WRITE 0x02
 
+#define PINNACLE_ERA_REG_X_AXIS_WIDE_Z_MIN 0x0149
+#define PINNACLE_ERA_REG_Y_AXIS_WIDE_Z_MIN 0x0168
 #define PINNACLE_ERA_REG_TRACKING_ADC_CONFIG 0x0187
 
 #define PINNACLE_TRACKING_ADC_CONFIG_1X 0x00
@@ -83,13 +86,21 @@ enum pinnacle_sensitivity {
     PINNACLE_SENSITIVITY_4X,
 };
 
+typedef int (*pinnacle_seq_read_t)(const struct device *dev, const uint8_t addr, uint8_t *buf,
+                                   const uint8_t len);
+typedef int (*pinnacle_write_t)(const struct device *dev, const uint8_t addr, const uint8_t val);
+
 struct pinnacle_config {
-#if DT_INST_ON_BUS(0, i2c)
-    const struct i2c_dt_spec bus;
-#elif DT_INST_ON_BUS(0, spi)
-    const struct spi_dt_spec bus;
-#endif
+    union {
+        struct i2c_dt_spec i2c;
+        struct spi_dt_spec spi;
+    } bus;
+
+    pinnacle_seq_read_t seq_read;
+    pinnacle_write_t write;
+
     bool rotate_90, sleep_en, no_taps;
     enum pinnacle_sensitivity sensitivity;
+    uint8_t x_axis_z_min, y_axis_z_min;
     const struct gpio_dt_spec dr;
 };
